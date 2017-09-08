c57c611338ece88e05832d064c388563e1fcd8e3
perfectsquircle <calvinfurano@gmail.com>
2017-09-07 22:23:39 -0600

    Rearrange sockets

--- routes/Poker.js
+++ routes/Poker.js
@@ -1,11 +1,27 @@
+const rooms = new Map();
+const PokerRoom = require("../lib/poker/PokerRoom");
+
 class Poker {
     constructor(app) {
-        app.get("/poker/:roomid", this.GetPoker);
+        app.get("/poker/:roomid", this.getPokerRoom.bind(this));
     }
 
-    GetPoker(req, res, next) {
-        console.log(req.params.roomid);
-        res.send("poker");
+    getPokerRoom(req, res, next) {
+        const roomId = req.params.roomid;
+        console.log();
+
+        if (rooms.has(roomId)) {
+            res.send("joined existing room: " + roomId);
+        } else {
+            this.createPokerRoom(roomId);
+            res.send("created room: " + roomId);
+        }
+    }
+
+    createPokerRoom(roomId) {
+        let room = new PokerRoom();
+        rooms.set(roomId, room);
+        return room;
     }
 }
 
