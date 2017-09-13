787d2d4c309ddd80c3bce958cf76ee3f80b83c8e
perfectsquircle <calvinfurano@gmail.com>
2017-09-12 20:54:03 -0600

    Move test data to server

--- routes/Poker.js
+++ routes/Poker.js
@@ -1,23 +1,21 @@
+var express = require('express');
+var router = express.Router();
 const rooms = require("../lib/poker/rooms")
 
-class Poker {
-    constructor(app) {
-        app.get("/poker/:roomid", this.getPokerRoom.bind(this));
+router.get("/poker/:roomid", (req, res, next) => {
+    const roomId = req.params.roomid;
+
+    if (!rooms.has(roomId)) {
+        rooms.add(roomId);
+        // return next();
     }
+    res.render("poker", { roomId })
+});
 
-    getPokerRoom(req, res, next) {
-        const roomId = req.params.roomid;
+router.get("/poker/:roomid/users", (req, res, next) => {
+    let room = rooms.get(req.params.roomid);
+    console.log(room, rooms);
+    res.json(room ? room.users : []);
+});
 
-        if (!rooms.has(roomId)) {
-            rooms.add(roomId);
-            // return next();
-        }
-        res.render("poker", { roomId })
-    }
-
-    createPokerRoom(roomId) {
-
-    }
-}
-
-module.exports = Poker;
+module.exports = router;
